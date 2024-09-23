199fd74d6bb8ee284a9985a825593b267baf1d4f
construktdev <construktai@gmail.com>
2024-09-23 14:52:03 +0200

    Code Clean-Ups

--- src/main/java/de/construkter/modules/embedBuilder/CommandListener.java
+++ src/main/java/de/construkter/modules/embedBuilder/CommandListener.java
@@ -4,7 +4,6 @@ import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.components.ActionRow;
-import net.dv8tion.jda.api.interactions.components.LayoutComponent;
 import net.dv8tion.jda.api.interactions.components.text.TextInput;
 import net.dv8tion.jda.api.interactions.components.text.TextInputStyle;
 import net.dv8tion.jda.api.interactions.modals.Modal;
