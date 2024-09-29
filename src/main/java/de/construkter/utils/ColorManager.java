2c40854eb15ec0bd726feef03e202c09ffec6389
construktdev <construktai@gmail.com>
2024-09-29 09:17:18 +0200

    Remove comment annotations for color codes
    Removed inline comment annotations for color codes in ColorManager.java. This cleans up the code and makes it more concise without losing functionality or readability.

--- src/main/java/de/construkter/utils/ColorManager.java
+++ src/main/java/de/construkter/utils/ColorManager.java
@@ -1,17 +1,17 @@
 package de.construkter.utils;
 
 public class ColorManager {
-    public static final String RESET = "\033[0m";  // Text Reset
-    public static final String BLACK = "\033[0;30m";   // BLACK
-    public static final String RED = "\033[0;31m";     // RED
-    public static final String GREEN = "\033[0;32m";   // GREEN
-    public static final String YELLOW = "\033[0;33m";  // YELLOW
-    public static final String BLUE = "\033[0;34m";    // BLUE
-    public static final String PURPLE = "\033[0;35m";  // PURPLE
-    public static final String CYAN = "\033[0;36m";    // CYAN
-    public static final String WHITE = "\033[0;37m";   // WHITE
+    public static final String RESET = "\033[0m";
+    public static final String BLACK = "\033[0;30m";
+    public static final String RED = "\033[0;31m";
+    public static final String GREEN = "\033[0;32m";
+    public static final String YELLOW = "\033[0;33m";
+    public static final String BLUE = "\033[0;34m";
+    public static final String PURPLE = "\033[0;35m";
+    public static final String CYAN = "\033[0;36m";
+    public static final String WHITE = "\033[0;37m";
 
-    public static final String BOLD = "\033[1m";    // BOLD
-    public static final String UNDERLINE = "\033[4m";   // UNDERLINE
+    public static final String BOLD = "\033[1m";
+    public static final String UNDERLINE = "\033[4m";
     public static final String REVERSED = "\033[7m";
 }
